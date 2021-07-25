faf99ffbf8025f5672e10780246a8ec2e147cdd1
Maciej Krawczyk <maciejro.krawczyk@gmail.com>
2021-07-25 17:52:36 +0200

    added controllers for user, and auth endpoints

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,20 +1,25 @@
 import { Module } from '@nestjs/common';
 import * as Joi from '@hapi/joi';
 import { ConfigModule } from '@nestjs/config';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       validationSchema: Joi.object({
         PORT: Joi.number().required(),
-        RABBITMQ_USER: Joi.string(),
-        RABBITMQ_PASSWORD: Joi.string(),
-        RABBITMQ_HOST: Joi.string(),
+        RABBITMQ_USER: Joi.string().required(),
+        RABBITMQ_PASSWORD: Joi.string().required(),
+        RABBITMQ_HOST: Joi.string().required(),
+        RABBITMQ_QUEUE_NAME: Joi.string().required(),
         SESSION_SECRET: Joi.string().required(),
         REDIS_HOST: Joi.string().required(),
         REDIS_PORT: Joi.number().required(),
       }),
     }),
+    AuthModule,
+    UsersModule,
   ],
 })
 export class AppModule {}
