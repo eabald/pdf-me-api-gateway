19780174c8ec1276b97ca2f37229c20ea661a655
Maciej Krawczyk <maciejro.krawczyk@gmail.com>
2021-09-28 21:46:49 +0200

    login process #2

--- src/main.ts
+++ src/main.ts
@@ -17,8 +17,10 @@ async function bootstrap() {
   const configService = app.get(ConfigService);
 
   app.enableCors({
-    origin: configService.get('FRONTEND_URL'),
-    credentials: true,
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
   });
 
   const RedisStore = createRedisStore(session);
