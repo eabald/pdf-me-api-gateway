51ccf604c3cde2792a0d145918afeae4184cf04a
Maciej Krawczyk <maciejro.krawczyk@gmail.com>
2021-10-16 19:42:06 +0200

    added get limits endpoint

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,26 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Inject,
+  UseGuards,
+  Req,
+  Get,
+  UseFilters,
+} from '@nestjs/common';
+import { ClientProxy } from '@nestjs/microservices';
+import { RequestWithUser } from '@pdf-me/shared';
+import { RpcExceptionFilter } from '../utils/rpcException.filter';
+import { CookieAuthenticationGuard } from '../auth/cookieAuthentication.guard';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  constructor(@Inject('LIMITS_SERVICE') private limitsService: ClientProxy) {}
+
+  @Get('/limit')
+  @UseFilters(RpcExceptionFilter)
+  @UseGuards(CookieAuthenticationGuard)
+  async getUserLimits(@Req() request: RequestWithUser) {
+    return await this.limitsService
+      .send({ cmd: 'limits-get-by-user' }, request.user.id)
+      .toPromise();
+  }
+}
